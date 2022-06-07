ed3e9a613f1ad3b49d1bc1da8b1da657c670156c
Srinaini2 <107024288+Srinaini2@users.noreply.github.com>
2022-06-07 19:18:00 +0530

    Add files via upload

---
+++ LearnImplicity.java
@@ -0,0 +1,25 @@
+package week2.day2;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnImplicity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativesubmit123")).click();
+		}
+}
